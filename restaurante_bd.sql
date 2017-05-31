49dac8e3ee6e20d0acb1330009ed59eaafc67e01
Igor Faustino <igorfaustino@alunos.utfpr.edu.br>
2017-05-31 18:13:15 -0300

    update sql

--- restaurante_bd.sql
+++ restaurante_bd.sql
@@ -40,7 +40,7 @@ create table funcionarios(
     salario float,
     situacao varchar(50) not null,
     nome varchar(50),
-    constraint pk_fun primary key  (idFun)
+    constraint pk_fun primary key  (cpf)
 );
 
 create table garcons(
@@ -61,10 +61,10 @@ create table pedidos(
 	idPed integer,
     situacao varchar(50),
     idCli integer not null,
-    idGar integer not null,
+    cpfGar integer not null,
     dataPed date,
     constraint pk_ped primary key (idPed),
-    constraint fk_pedGar foreign key (idGar) references garcons(idFun),
+    constraint fk_pedGar foreign key (cpfGar) references garcons(cpf),
     constraint fk_cli foreign key (idCli) references clientes(idCli)
 );
 
@@ -120,16 +120,13 @@ insert into mesas(nroMesa, nroPessoas) values (1, 4), (2, 4), (3, 8), (4, 8), (5
 insert into clientes(idCli, nome, telefone, cpf) value (101, 'Astolfo da Silva', '99999-9999', '121312312'), (903, 'John Smith', '07700 900461', '3454523'), (1200, 'Mat Smith', '07700 900461', '45898989'), (2000, 'Peter Capaldi', '07700 900461', '78797979');
 insert into reservas(idReser, idCli, datas, hora, nroPessoas) values (10, 101, '2017-10-12', '20:00:00', 2), (11, 2000, '1998-10-15', '17:00:00', 2), (12, 2000, '3012-10-15', '17:00:00', 3);
 insert into reservas_mesas(idReser, nroMesa) values (10, 2), (11, 3), (12, 2);
-insert into funcionarios(idFun, salario, situacao, nome) values (1, 3000, 'Ativo', 'Master chefe'), (2, NULL, 'Aposentado', 'Mario Verde'), (3, 1000, 'Ativo', 'Mario Mario'), (4, NULL, 'Estagiario', 'Luigi Mario');
-insert into garcons(idFun) values (1), (2);
-insert into cozinheiros (idFun, idChefe) values (3, NULL), (4, 3);
-insert into pedidos (idPed, situacao, idCli, idGar, dataPed) values (1, 'pendente', 2000, 1, CURDATE()), (2, 'pago', 2000, 2, CURDATE());
+insert into funcionarios(cpf, salario, situacao, nome) values (1, 3000, 'Ativo', 'Master chefe'), (2, NULL, 'Aposentado', 'Mario Verde'), (3, 1000, 'Ativo', 'Mario Mario'), (4, NULL, 'Estagiario', 'Luigi Mario');
+insert into garcons(cpf) values (1), (2);
+insert into cozinheiros (cpf, cpfChefe) values (3, NULL), (4, 3);
+insert into pedidos (idPed, situacao, idCli, cpfGar, dataPed) values (1, 'pendente', 2000, 1, CURDATE()), (2, 'pago', 2000, 2, CURDATE());
 insert into pratos (id, nome) values (1, 'Macarrao instantaneo'), (2, 'Pizza congelada'), (3, 'Milk Shake');
 insert into pedidos_pratos (idPratos, idPed, qtd) values (1, 1, 3), (2, 1, 1), (3, 1, 7);
 insert into ingredientes (id, nome, qtdTotal) values (1, 'Cebola', 7), (2, 'Chocolate', 3);
 insert into pratos_ingrediente (idIng, idPratos, qtd) values (2, 3, 2);
 insert into fornecedores (id, nome, telefone) values (10, 'Bob da Silva', '99999-9999'), (12, 'Carlos da Silva', '99999-9900'), (15, 'Bob Smith', '99999-0000');
 insert into ingredientes_fornecedores (idForn, idIng) values (10, 2), (12, 2), (15, 2), (10, 1);
-
-select *
-from clientes;
