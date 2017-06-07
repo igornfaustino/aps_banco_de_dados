a8bc922b6a83caa08ef5bef21ca3ffe4cf9c61a5
igorFaustino <igornfaustino@gmail.com>
2017-06-06 23:45:31 -0300

    templates python

--- app.py
+++ app.py
@@ -10,23 +10,25 @@ def index():
 
 # Menus
 
-@app.route('/clientes')
-def clientes():
+@app.route('/cliente')
+def cliente():
 	return render_template('clientes.html')
 
-@app.route('/funcionarios')
-def funcionarios():
+@app.route('/funcionario')
+def funcionario():
 	return render_template('funcionarios.html')
 
 @app.route('/restaurante')
 def restaurante():
 	return render_template('restaurante.html')
 
-@app.route('/pedidos')
-def pedidos():
+@app.route('/pedido')
+def pedido():
 	return render_template('pedidos.html')
 
 # fim Menus
+
+# crud clientes
 
 @app.route('/cliente/cadastro')
 def cadastro_cliente():
@@ -41,11 +43,79 @@ def cliente_submit():
 	tel = request.form['tel_cliente']
 	cpf = request.form['cpf_cliente']
 	print (nome, tel, cpf)
-	return redirect(url_for('clientes'))
+	return redirect(url_for('cliente'))
 
-@app.route('/search')
+@app.route('/cliente/search')
 def search_cliente():
 	return render_template('selecionar_cliente.html')
+
+@app.route('/cliente/results')
+def results_cliente_nome():
+	return render_template('listar_resultados.html')
+
+@app.route('/cliente/results')
+def results_cliente_algo():
+	pass
+
+#crud pedidos
+
+@app.route('/pedido/cadastro')
+def cadastro_pedido():
+	return render_template('cadastro/cadastro_pedido.html')
+
+@app.route('/cadastro/pedido/submit', methods=['POST'])
+def pedido_submit():
+	nroMesa = None
+	nroMesa = request.form['nroMesa']
+	print (nroMesa)
+	return redirect(url_for('pedido'))
+
+@app.route('/pedido/search')
+def search_pedido():
+	return render_template('selecionar_pedido.html')
+
+@app.route('/pedido/results')
+def results_pedido_nome():
+	return render_template('listar_resultados.html')
+
+@app.route('/pedido/results')
+def results_pedido_algo():
+	pass
+
+#crud funcionarios
+
+@app.route('/funcionario/cadastro')
+def cadastro_funcionario():
+	return render_template('cadastrar_funcionarios.html')
+
+@app.route('/funcionario/cadastro/cozinheiro')
+def cadastro_cozinheiro():
+	return render_template('cadastro/cadastro_cozinheiro.html')
+
+@app.route('/cadastro/funcionario/submit/<funcao>', methods=['POST'])
+def funcionario_submit(funcao):
+	nome_funcionario = None
+	tel_funcionario = None
+	cpf_funcionario = None
+
+	if funcao == "cozinheiro":
+		print ("Sou um cozinheiro \n\n")
+	else:
+		pass
+	return redirect(url_for('funcionario'))
+
+@app.route('/funcionario/search')
+def search_funcionario():
+	return render_template('selecionar_funcionario.html')
+
+@app.route('/funcionario/results')
+def results_funcionario_nome():
+	return render_template('listar_resultados.html')
+
+@app.route('/funcionario/results')
+def results_funcionario_algo():
+	pass
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
