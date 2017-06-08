69108d334a953d3f3185111fb20c75a0de45bab3
igorFaustino <igornfaustino@gmail.com>
2017-06-07 22:39:54 -0300

    bdHelper

--- bdHelper.py
+++ bdHelper.py
@@ -8,17 +8,27 @@ class bdHelper():
 	def connect(self, database="restaurante_bd"):
 		return pymysql.connect(host="localhost", user="root", passwd="root", db=database)
 
-	def get_all_inputs(self):
+	def cadastro_cliente(self, idCli=None, nome=None, telefone=None, cpf=None):
 		connection = self.connect()
 		try:
-			query = "select * from clientes;"
+			query = "insert into clientes(idCli, nome, telefone, cpf) values(%s, %s, %s, %s);"
+			
 			with connection.cursor() as cursor:
-				cursor.execute(query)
-				return cursor.fetchall()
+				cursor.execute(query, (idCli, nome, telefone, cpf))
+				connection.commit()
+		except Exception as e:
+			print(e)
 		finally:
 			connection.close()
 
+	def cadastro_pedido(self, nroMesa):
+		connection = self.connect()
+		try:
+			query = "insert into pedidos"
+
+	
+
 if __name__ == '__main__':
 	teste = bdHelper()
-	data = teste.get_all_inputs()
-	print(data)
+	teste.cadastro_cliente("7", "David Tennant", "183", "12334")
+	# print(data)
